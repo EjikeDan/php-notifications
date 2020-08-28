c015dfb0681449461a54f43a3ee8b48641c9a0a3
EjikeDan <ejikedozie@gmail.com>
2020-08-28 02:35:50 +0100

    updated db

--- database.php
+++ database.php
@@ -1,3 +1,3 @@
 <?php 
-$con=mysqli_connect("localhost","root","root","trip") or die('Database not connected');
+$con=mysqli_connect("db4free.net","ejikedozie","agkdandozie","trip_database") or die('Database not connected');
 ?>
